e9f4c68b66314b3cd70dff402c8976a0ce18b1b7
Arthur <pyeye.91@gmail.com>
2021-02-11 14:10:40 +0500

    reset index on reindex products

--- apps/products/management/commands/reindex_products.py
+++ apps/products/management/commands/reindex_products.py
@@ -6,6 +6,8 @@ from apps.products.models import ProductInfo
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        elastic.delete_index()
+        elastic.create_index()
         products = ProductInfo.objects.all()
         for product in products:
             elastic.index_products(product)
